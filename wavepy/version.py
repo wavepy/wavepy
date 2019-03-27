27dce55f4db3f272a551bf37eb25ab8495a79411
lucarebuffi <lrebuffi@anl.gov>
2019-03-27 15:13:03 -0500

    First Commit First Commit

---
+++ wavepy/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM wavepy SETUP.PY
+short_version = '1.0.3'
+version = '1.0.3'
+full_version = '1.0.3'
+git_revision = '93aca353e9cac0459f3b0afed6b5bb706229d924'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
