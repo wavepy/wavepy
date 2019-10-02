8cc237615cb5d02ed731dd000e7e13f181758865
Zhi.Qiao <40144800+QDLDQQiao@users.noreply.github.com>
2019-10-02 12:04:40 -0500

    Zhi test

--- wavepy/version.py
+++ wavepy/version.py
@@ -3,7 +3,7 @@
 short_version = '1.0.4'
 version = '1.0.4'
 full_version = '1.0.4'
-git_revision = '46d7230298fdbfea053967f0a04d0b73eb6876ac'
+git_revision = 'ffb246697096ac99ba127f0bd185e5b9e019c1ad'
 release = True
 
 if not release:
