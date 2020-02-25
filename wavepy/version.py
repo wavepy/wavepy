d846990f99402cd342e9d58a344b46606e16ded5
lucarebuffi <lrebuffi@anl.gov>
2020-02-25 13:29:53 -0600

    First Commit: Refactored version

--- wavepy/version.py
+++ wavepy/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM wavepy SETUP.PY
-short_version = '1.0.4'
-version = '1.0.4'
-full_version = '1.0.4'
-git_revision = 'e9d6e612f369604fa6b2d0bb17e29ac19d59e1be'
+short_version = '1.0.5'
+version = '1.0.5'
+full_version = '1.0.5'
+git_revision = '16373e158daaa30655867d15e16e3949de269915'
 release = True
 
 if not release:
